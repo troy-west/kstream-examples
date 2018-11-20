6966e7895fda54b21f5dbbe304d3ab2d59480791
Derek Troy-West <derek@troywest.com>
2018-11-20 15:46:33 +1100

    tumbling aggregations

--- test/racing/fixture_test.clj
+++ test/racing/fixture_test.clj
@@ -6,7 +6,15 @@
            (org.apache.kafka.streams.kstream Predicate TimeWindows Windowed KeyValueMapper Reducer Initializer Aggregator)
            (org.apache.kafka.streams.test ConsumerRecordFactory)
            (org.apache.kafka.clients.producer ProducerRecord)
-           (org.apache.kafka.common.serialization StringDeserializer StringSerializer)))
+           (org.apache.kafka.common.serialization StringDeserializer StringSerializer Serializer)))
+
+;; Test config is static across all our tests
+(def config (let [props (Properties.)]
+              (.putAll props {"application.id"      "test-racing-fixtures"
+                              "bootstrap.servers"   "dummy:1234"
+                              "default.key.serde"   "org.apache.kafka.common.serialization.Serdes$StringSerde"
+                              "default.value.serde" "serdes.JsonSerde"})
+              props))
 
 (defn read-output
   [driver topic]
@@ -17,84 +25,88 @@
                                  (serdes/->JsonDeserializer))]
     (.value ^ProducerRecord record)))
 
-;; We create some boilerplate code for testing Kafka Streams
-(let [factory (ConsumerRecordFactory. "fixtures"
-                                      (StringSerializer.)
-                                      (serdes/->JsonSerializer)
-                                      (.getTime (Date.))
-                                      6000)
-      config  (let [props (Properties.)]
-                (.putAll props {"application.id"      "test-racing-fixtures"
-                                "bootstrap.servers"   "dummy:1234"
-                                "default.key.serde"   "org.apache.kafka.common.serialization.Serdes$StringSerde"
-                                "default.value.serde" "serdes.JsonSerde"})
-                props)]
+(deftest filter-closed
 
-  (deftest filter-closed
+  (let [start-time (.getTime (Date.))                       ;; the test start time
+        factory    (ConsumerRecordFactory. "fixtures"       ;; A factory that auto-advances its internal time
+                                           (StringSerializer.)
+                                           (serdes/->JsonSerializer)
+                                           start-time
+                                           6000)
 
-    ;; We create a builder and use it to define a topology, in this case filtering closed fixtures to a new topic.
-    (let [builder (StreamsBuilder.)]
+        ;; We create a builder and use it to define a topology.
+        builder    (StreamsBuilder.)]
 
-      (-> (.stream builder "fixtures")
-          (.filter (reify Predicate
-                     (test [_ _ event]
-                       (true? (= "closed" (:state event))))))
-          (.to "closed-fixtures"))
+    ;; In this case filtering closed fixtures to a new topic.
+    (-> (.stream builder "fixtures")
+        (.filter (reify Predicate
+                   (test [_ _ event]
+                     (true? (= "closed" (:state event))))))
+        (.to "closed-fixtures"))
 
-      (with-open [driver (TopologyTestDriver. (.build builder) config)]
+    (with-open [driver (TopologyTestDriver. (.build builder) config)]
 
-        ;; Test that by sending the input topic multiple events
-        (let [event-1 (.create factory "fixtures" "race-1" {:id "race-1" :state "open"})
-              event-2 (.create factory "fixtures" "race-1" {:id "race-1" :state "open"})
-              event-3 (.create factory "fixtures" "race-2" {:id "race-2" :state "open"})
-              event-4 (.create factory "fixtures" "race-1" {:id "race-1" :state "open"})
-              event-5 (.create factory "fixtures" "race-1" {:id "race-1" :state "closed"})
-              event-6 (.create factory "fixtures" "race-3" {:id "race-3" :state "open"})]
+      ;; Test that by sending the input topic multiple events
+      (let [event-1 (.create factory "fixtures" "race-1" {:id "race-1" :state "open"})
+            event-2 (.create factory "fixtures" "race-1" {:id "race-1" :state "open"})
+            event-3 (.create factory "fixtures" "race-2" {:id "race-2" :state "open"})
+            event-4 (.create factory "fixtures" "race-1" {:id "race-1" :state "open"})
+            event-5 (.create factory "fixtures" "race-1" {:id "race-1" :state "closed"})
+            event-6 (.create factory "fixtures" "race-3" {:id "race-3" :state "open"})]
 
-          ;; and checking that the closed-fixtures topic contains only the closed event
-          (is (= {:id "race-1" :state "closed"}
-                 (do (.pipeInput driver event-1)
-                     (.pipeInput driver event-2)
-                     (.pipeInput driver event-3)
-                     (.pipeInput driver event-4)
-                     (.pipeInput driver event-5)
-                     (.pipeInput driver event-6)
-                     (read-output ^TopologyTestDriver driver "closed-fixtures"))))))))
+        ;; and checking that the closed-fixtures topic contains only the closed event
+        (is (= {:id "race-1" :state "closed"}
+               (do (.pipeInput driver event-1)
+                   (.pipeInput driver event-2)
+                   (.pipeInput driver event-3)
+                   (.pipeInput driver event-4)
+                   (.pipeInput driver event-5)
+                   (.pipeInput driver event-6)
+                   (read-output ^TopologyTestDriver driver "closed-fixtures"))))))))
 
-  (deftest hopping-time-windows
+(deftest tumbling-aggregation-with-specific-time-advancement
 
-    ;; In this test we'll start to play with hopping-time-windows to explore how they work
-    (let [builder (StreamsBuilder.)]
+  ;; In this test we'll start to play with hopping-time-windows to explore how they work
+  (let [start-time (.getTime (Date.))                       ;; the test start time
+        factory    (ConsumerRecordFactory. "fixtures"       ;; A factory that *does not* auto advance time
+                                           ^Serializer (StringSerializer.)
+                                           ^Serializer (serdes/->JsonSerializer)
+                                           start-time)
+        builder    (StreamsBuilder.)]
 
-      (-> (.stream builder "fixtures")
-          (.groupByKey)
-          (.windowedBy (TimeWindows/of 20000))
-          (.aggregate (reify Initializer
-                        (apply [_] []))
-                      (reify Aggregator
-                        (apply [_ _ event agg]
-                          (conj agg event))))
-          (.toStream (reify KeyValueMapper
-                       (apply [_ k _]
-                         (.key ^Windowed k))))
-          (.to "closed-fixtures"))
+    (-> (.stream builder "fixtures")
+        (.groupByKey)
+        (.windowedBy (-> (TimeWindows/of 20000)
+                         (.until 20000)))
+        (.aggregate (reify Initializer
+                      (apply [_] []))
+                    (reify Aggregator
+                      (apply [_ _ event agg]
+                        (conj agg event))))
+        (.toStream (reify KeyValueMapper
+                     (apply [_ k _]
+                       (.key ^Windowed k))))
+        (.to "closed-fixtures"))
 
-      (with-open [driver (TopologyTestDriver. (.build builder) config)]
+    (with-open [driver (TopologyTestDriver. (.build builder) config start-time)]
 
-        ;; Test that by sending the input topic multiple events
-        (let [event-1 (.create factory "fixtures" "race-1" {:id "race-1" :state "open"})
-              event-2 (.create factory "fixtures" "race-1" {:id "race-1" :state "open" :attr "x"})
-              event-3 (.create factory "fixtures" "race-1" {:id "race-1" :state "open" :attr "y"})
-              event-4 (.create factory "fixtures" "race-1" {:id "race-1" :state "open" :attr "z"})]
+      (.pipeInput driver (.create factory "fixtures" "race-1" {:id "race-1" :state "open"}))
 
-          ;; and checking that the closed-fixtures topic contains only the closed event
-          (is (= [{:id "race-1" :state "open"}]
-                 (do (.pipeInput driver event-1)
-                     (.pipeInput driver event-2)
-                     (.pipeInput driver event-3)
-                     (.pipeInput driver event-4)
-                     [(read-output ^TopologyTestDriver driver "closed-fixtures")
-                      (read-output ^TopologyTestDriver driver "closed-fixtures")
-                      (read-output ^TopologyTestDriver driver "closed-fixtures")
-                      (read-output ^TopologyTestDriver driver "closed-fixtures")
-                      ]))))))))
+      (.advanceWallClockTime driver 100)
+      (.advanceTimeMs factory 100)
+      (.pipeInput driver (.create factory "fixtures" "race-1" {:id "race-1" :state "open" :x 1}))
+
+      ;; this is a new tumbling window / aggregation
+      (.advanceWallClockTime driver 20000)
+      (.advanceTimeMs factory 20000)
+      (.pipeInput driver (.create factory "fixtures" "race-1" {:id "race-1" :state "open" :x 2}))
+
+      (is (= [[{:id "race-1" :state "open"}]
+
+              [{:id "race-1" :state "open"}
+               {:id "race-1" :state "open" :x 1}]
+
+              [{:id "race-1" :state "open" :x 2}]]
+             [(read-output ^TopologyTestDriver driver "closed-fixtures")
+              (read-output ^TopologyTestDriver driver "closed-fixtures")
+              (read-output ^TopologyTestDriver driver "closed-fixtures")])))))
